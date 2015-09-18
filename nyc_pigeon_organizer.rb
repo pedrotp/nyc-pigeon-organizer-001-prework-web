d3acd35f020265a27bb3e845cea4f4f2b10d5b0d
pedrotp <pedro@pedrotp.com>
2015-09-18 13:23:46 -0400

    meet the pigeons

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
+pigeon_data = {
+        :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+      }
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  data.each do |attribute, values|
+    values.each do |value, names|
+      names.each do |name|
+        if organized_data.include?(name) == false
+          organized_data[name] = {attribute => [value.to_s]}
+        else
+          if organized_data[name].include?(attribute) == false
+            organized_data[name][attribute] = [value.to_s]
+          else  
+            organized_data[name][attribute] << value.to_s
+          end
+        end
+      end
+    end
+  end
+  organized_data
 end
